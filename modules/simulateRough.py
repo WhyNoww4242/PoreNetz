28a694ebfb311959e86d1f848a227fd5a1a6d2e2
WhyNoww4242 <brycerives@gmail.com>
2025-12-24 13:48:28 -0700

    test multi ion

--- modules/simulateRough.py
+++ modules/simulateRough.py
@@ -9,10 +9,16 @@ class Space():
     """
     Docstring for Space
     """
-    def __init__(self, Domain, tDomain, val):
+    def __init__(self, Domain, tDomain, initial_vec):
         self.Domain = Domain
         self.tDomain = tDomain
+        self.empty = jnp.zeros((Domain.size,tDomain.size))
         self.val = val
+
+        self.cp = jnp.zeros_like(self.empty)
+        self.cm = jnp.zeros_like(self.empty)
+        self.phi = jnp.zeros_like(self.empty)
+        print('print',self.cp.nbytes)
 
     @jit
     def solver(self):
@@ -27,24 +33,15 @@ class Space():
     def _tree_unflatten(cls, aux_data, children):
         return cls(*children, **aux_data)
         
-    
-"""
-  def _tree_flatten(self):
-    children = (self.x,)  # arrays / dynamic values
-    aux_data = {'mul': self.mul}  # static values
-    return (children, aux_data)
-
-  @classmethod
-  def _tree_unflatten(cls, aux_data, children):
-    return cls(*children, **aux_data)"""
 
 if __name__ == "__main__":
-    z_Space = jnp.linspace(0, 1, 100)
-    t_Space = jnp.linspace(0, 1, 10_000)
+    z_Space = jnp.linspace(0, 1, 1000)
+    t_Space = jnp.linspace(0, 1, 100_000)
 
     tree_util.register_pytree_node(Space,
                                Space._tree_flatten,
                                Space._tree_unflatten)
 
     poreTest = Space(z_Space, t_Space, 0)
+    
     print(poreTest.solver())
